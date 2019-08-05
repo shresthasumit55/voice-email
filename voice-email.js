d0799c57ff47f9f892b118aef96e316781bb9cfd
sanjel <arun_sanjel1@baylor.edu>
2019-08-05 16:08:38 -0500

    UI design improved

--- voice-email.js
+++ voice-email.js
@@ -9,3 +9,33 @@ $("#switch").click(function(){
         mode = 0;
     }
 });
+
+$("#btnInc").click(function(){
+	var fontSize = parseInt($("#main").css("font-size"));
+	var fontSize2 = parseInt($(".tabButtons").css("font-size"))
+	fontSize = fontSize + 1 + "px";
+	
+	$("#main").css({'font-size':fontSize});
+	
+});
+
+$("#btnDec").click(function(){
+	var fontSize = parseInt($("#main").css("font-size"));
+	fontSize = fontSize - 1 + "px";
+	$("#main").css({'font-size':fontSize});
+	
+});
+
+$(document).ready(function(){
+    // Hides all new badges
+    var noEmail = $(".new-email-badge");
+    console.log(noEmail.hide());
+    addNew(); 
+})
+// Adds "NEW" badge left of an unread email or new email 
+function addNew(){
+   var elements= $(".unread");
+   var newEmail = $(".new-email-badge");
+
+    console.log(elements.find(newEmail).show());
+}
